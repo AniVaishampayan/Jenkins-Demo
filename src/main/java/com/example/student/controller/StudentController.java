326d0c805789edd4911a47877d62564c3b28a245
AniVaishampayan <aniket.vaishampayan@avisys.in>
2024-02-28 18:41:50 +0530

    new-changes-added

--- src/main/java/com/example/student/controller/StudentController.java
+++ src/main/java/com/example/student/controller/StudentController.java
@@ -1,52 +1,24 @@
 package com.example.student.controller;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.student.entity.Student;
-import com.example.student.repository.StudentRepository;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
-    @Autowired
-    private StudentRepository studentRepository;
-
-    @GetMapping("/list")
-    public List<Student> getAllStudents() {
-        return studentRepository.findAll();
-    }
-
-    @PostMapping("/save")
-    public Student createStudent(@RequestBody Student student) {
-        return studentRepository.save(student);
-    }
+ 
+	 @PostMapping("/Saving")
+	    public String createTest() {
+	        return "POST request call successfully";
+	    }
+	 
+	    @DeleteMapping("/Delete")
+	    public String deleteTest() {
+	        return "DELETE request call successfully";
+	    }
     
-    @GetMapping("/{id}")
-    public Optional<Student> getStudentById(@PathVariable Long id) {
-       return studentRepository.findById(id);                
-    } 
-    
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteStudentById(@PathVariable Long id) {
-        Optional<Student> studentOptional = studentRepository.findById(id);
-        if (studentOptional.isPresent()) {
-            studentRepository.deleteById(id);
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
 }
